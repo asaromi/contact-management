11e63c59eb3353e7ab8bd41a53aadaf1a52816c9
Asaromi <a.sabililromi@gmail.com>
2023-11-13 04:52:48 +0700

    feat: middleware - define api auth middleware to detect logged in user

---
+++ app/Http/Middleware/ApiAuthMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->header('Authorization');
+        $authenticate = true;
+        $user = null;
+
+        if (!$token) $authenticate = false;
+        else {
+            $user = User::query()->where('token', $token)->first()->makeHidden('token');
+
+            if (!$user) $authenticate = false;
+        }
+
+        if (!$authenticate) {
+            return response()->json([
+                'error' => [
+                    'message' => ['Unauthorized']
+                ]
+            ])->setStatusCode('401');
+        }
+
+        Auth::login($user);
+        return $next($request);
+    }
+}
