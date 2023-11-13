65f3b7479f4cda5ae4ffe59e408c39fedc258a9b
Asaromi <a.sabililromi@gmail.com>
2023-11-13 19:32:09 +0700

    feat: route - add get current auth endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn() => ['message' => 'Hello World!']);
 Route::post('/users', [\App\Http\Controllers\UserController::class, 'register']);
 Route::post('/users/login', [\App\Http\Controllers\UserController::class, 'login']);
+
+Route::group(['middleware' => \App\Http\Middleware\ApiAuthMiddleware::class], function () {
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/current', [\App\Http\Controllers\UserController::class, 'get']);
+    });
+});
