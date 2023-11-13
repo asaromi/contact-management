dabf55cf3496aaf8996cf3831549e70d345ca2bd
Asaromi <a.sabililromi@gmail.com>
2023-11-13 19:31:13 +0700

    fix: test - remove refresh data users for each running test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,6 +14,5 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
         DB::delete("delete from addresses");
         DB::delete("delete from contacts");
-        DB::delete("delete from users");
     }
 }
